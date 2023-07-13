310c3c61e8d0d4790e74fa25db4caecb6e0afb9e
Dnyanesh Dasare <dnyanesh.dasare@gmail.com>
2023-07-13 20:56:49 +0530

    Library App

--- src/libapp/LibraryApp.java
+++ src/libapp/LibraryApp.java
@@ -14,11 +14,24 @@ public class LibraryApp {
     public void searchByIsbn(String isbn) {
         System.out.printf("Searching for books with ISBN %s.\n", isbn);
         Book book = bookRepo.findByIsbn(isbn);
-        if ( book != null ) {
-            System.out.printf("Book found:\n\tTitle: %s \n\tGenre: %s\n\tAuthor: %s\n\t",book.getTitle(),book.getGenre(),book.getAuthor());
-        }
-        else {
-            System.out.printf("\nSorry! Could not find book with ISBN %s", isbn);
+        respondWithResult(book, "ISBN");
+    }
+
+    public void searchByTitle(String title) {
+        Book book = bookRepo.findByTitle(title);
+        respondWithResult(book, "Title");
+    }
+
+    public void searchByAuthor(String author) {
+        Book book = bookRepo.findByAuthor(author);
+        respondWithResult(book, "Author");
+    }
+
+    public void respondWithResult(Book book, String searchType) {
+        if (book != null) {
+            System.out.printf("Book found:\n\tTitle: %s \n\tGenre: %s\n\tAuthor: %s\n\t", book.getTitle(), book.getGenre(), book.getAuthor());
+        } else {
+            System.out.printf("\nSorry! Could not find book with this %s.", searchType);
         }
     }
 }
--- src/libapp/Main.java
+++ src/libapp/Main.java
@@ -1,6 +1,42 @@
 package libapp;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Welcome to the library app");
+        LibraryApp app = new LibraryApp();
+        System.out.print("Search by(isbn/title/author): ");
+        String searchBy = keyboard.next();
+
+        switch (searchBy.toLowerCase()) {
+            case "isbn":
+                System.out.println("____________________________");
+                System.out.println("Searching by isbn");
+                System.out.print("Please enter the isbn number: ");
+                String isbn = keyboard.next();
+                app.searchByIsbn(isbn);
+                break;
+            case "title":
+                System.out.println("____________________________");
+                System.out.println("Searching by title:");
+                System.out.print("Please enter the title ");
+                String title = keyboard.next();
+                app.searchByTitle(title);
+                break;
+            case "author":
+                System.out.println("____________________________");
+                System.out.println("Searching by author");
+                System.out.print("Please enter the author name: ");
+                String author = keyboard.next();
+                app.searchByAuthor(author);
+                break;
+            default:
+                System.out.println("____________________________");
+                System.out.println("Wrong search criteria");
+                break;
+        }
     }
 }
