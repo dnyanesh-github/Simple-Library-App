f51465a2aeb8b717d25d55294875d6138570e641
Dnyanesh Dasare <dnyanesh.dasare@gmail.com>
2023-07-13 09:50:21 +0530

    Library App

--- src/libapp/Main.java
+++ src/libapp/Main.java
@@ -1,9 +1,6 @@
 package libapp;
-
 public class Main {
     public static void main(String[] args) {
-        LibraryApp app = new LibraryApp();
-        app.searchByIsbn("isbn123456");
+        System.out.println("Hello world!");
     }
-
 }
